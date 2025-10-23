1ad456cae6355bfe3170fbbbe8aa279e64a830a8
Dustin <6962246+djdarcy@users.noreply.github.com>
2025-10-22 20:18:35 -0400

    Updating Smart Resolution Calculator workflow example image

--- version.py
+++ version.py
@@ -27,7 +27,7 @@ PHASE = "alpha"  # Options: None, "alpha", "beta", "rc1", "rc2", etc.
 # DO NOT EDIT THIS LINE MANUALLY
 # Note: Hash reflects the commit this version builds upon (HEAD at commit time)
 # The hash will be one commit behind after the commit is created (git limitation)
-__version__ = "0.1.3-alpha_main_25-20251022-437349a"
+__version__ = "0.1.3-alpha_main_30-20251022-1e856f8"
 
 
 def get_version():
